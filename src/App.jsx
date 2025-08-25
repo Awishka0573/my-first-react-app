e1df3cfdf1671e954d43e03f86c1c36fc680e50c
Awishka0573 <awishkaisuru0573@gmail.com>
2025-08-25 23:39:19 +0530

    refactor: enhance Card component inline styling and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 const Card = ({title}) => {
   return (
-    <div>
+    <div className="card"style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#313663',
+      borderRadius:'10px',
+      minHeight:'100px'
+    }}>
       <h2>{title}</h2>
     </div>
   )
