8048347494d7f55d1f54e1614f8246fb102f49a5
Awishka0573 <awishkaisuru0573@gmail.com>
2025-08-26 22:31:43 +0530

    refactor: enhance Card component with like functionality and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const Card = ({title}) => {
-  const [hasLiked , setHasLiked] = useState(false);
-  return (
-    <div className="card">
-      <h2>{title}</h2>
 
-      <button onClick={() => setHasLiked(true)}> 
+  const [count,setCount] = useState(0);
+  const [hasLiked , setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log('${title} has been liked  : ${hasLiked}');
+  },  [hasLiked, title]);
+
+
+  return (
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} <br /> {count} </h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}> 
         {hasLiked ? "💌" : "❤"}
         
       </button> 
@@ -29,33 +37,5 @@ const App = () => {
   )
 }
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 export default App;
