921e631ea23a21f443ae4f4def59b89d0ded5af0
Awishka0573 <awishkaisuru0573@gmail.com>
2025-08-27 13:29:26 +0530

    feat: integrate Tailwind CSS and refactor App component
    - Added Tailwind CSS dependencies to package.json and configured Vite to use Tailwind.
    - Refactored App component to display a simple "Hello world!" message styled with Tailwind.
    - Removed Card component and associated logic for a cleaner initial setup.
    - Updated index.css to use Tailwind and removed custom styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,82 @@
-import { useEffect, useState } from 'react'
-
-
-const Card = ({title}) => {
-
-  const [count,setCount] = useState(0);
-  const [hasLiked , setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log('${title} has been liked  : ${hasLiked}');
-  },  [hasLiked, title]);
-
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br /> {count} </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}> 
-        {hasLiked ? "💌" : "❤"}
-        
-      </button> 
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-
-
   return (
-    <div className="card-container">
-      <Card title="I" />
-      <Card title="am" />
-      <Card title="Awishka" />
-      <Card title="Isuru"/>
-      <Card title="Kumara"/>
-    </div>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500 ">
+         Hello world!
+      </h1>
+  </div>
   )
 }
 
+export default App
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from 'react'
+
+
+// const Card = ({title}) => {
+
+//   const [count,setCount] = useState(0);
+//   const [hasLiked , setHasLiked] = useState(false);
+
+//   useEffect(() => {
+//     console.log('${title} has been liked  : ${hasLiked}');
+//   },  [hasLiked, title]);
+
+
+//   return (
+//     <div className="card" onClick={() => setCount(count + 1)}>
+//       <h2>{title} <br /> {count || } </h2>
+
+//       <button onClick={() => setHasLiked(!hasLiked)}> 
+//         {hasLiked ? "💌" : "❤"}
+        
+//       </button> 
+//     </div>
+//   )
+// }
+
+// const App = () => {
+
+
+//   return (
+//     <div className="card-container">
+//       <Card title="I" />
+//       <Card title="am" />
+//       <Card title="Awishka" />
+//       <Card title="Isuru"/>
+//       <Card title="Kumara"/>
+//     </div>
+//   )
+// }
+
+
+// export default App;
